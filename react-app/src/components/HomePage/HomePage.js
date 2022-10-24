179e69b81090b76c2dafbdd591fa639731d18453
Nathan <nscott1010@gmail.com>
2022-10-23 20:14:11 -0500

    homepage begun

---
+++ react-app/src/components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import "./HomePage.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
+
+export function HomePage() {
+    const user = useSelector(state => state.session.user);
+
+        return (
+            <>
+                <div className="background">
+
+                </div>
+            </>
+        )
+}
