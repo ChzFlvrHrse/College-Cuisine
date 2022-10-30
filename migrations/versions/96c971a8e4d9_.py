165144750cd875159ccd8c787114da6248828ced
Nathan <nscott1010@gmail.com>
2022-10-29 20:29:41 -0500

    db migration

--- migrations/versions/96c971a8e4d9_.py
+++ migrations/versions/96c971a8e4d9_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: f0a09767e46e
+Revision ID: 96c971a8e4d9
 Revises: 
-Create Date: 2022-10-29 19:57:11.008814
+Create Date: 2022-10-29 20:28:31.434596
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f0a09767e46e'
+revision = '96c971a8e4d9'
 down_revision = None
 branch_labels = None
 depends_on = None
