04a6c1ad0d6a30fe7de524ecfe2876496b38109b
Nathan <nscott1010@gmail.com>
2022-10-31 18:42:13 -0500

    404 page done

--- react-app/src/components/UnknownPage/404.js
+++ react-app/src/components/UnknownPage/404.js
@@ -3,7 +3,7 @@ import "./404.css";
 import { login } from '../../store/session';
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
 export function PageNotFound() {
@@ -18,27 +18,27 @@ export function PageNotFound() {
     await dispatch(login('demo@aa.io', 'password'))
     history.push('/');
   };
-if (!user) {
+  if (!user) {
 
-  return (
-    <div className="login-containerPNF">
-      <div className="inner-loginPNF">
-        <div id="login-bannerPNF">
-          {/* <div id="circles-containerPNF">
+    return (
+      <div className="login-containerPNF">
+        <div className="inner-loginPNF">
+          <div id="login-bannerPNF">
+            {/* <div id="circles-containerPNF">
             <img src={klickrLogo} alt="" id="circlesPNF"></img>
           </div> */}
-          <h4 id="to-klickr1">Sign in Required</h4>
+            <h4 id="to-klickr1">Sign in Required</h4>
             <div className="button">
               <button className="loginform-bttns" onClick={onLogin}>
                 Click here to login as Demo User
               </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
-else return (
+    );
+  }
+  else return (
     <div className="login-containerPNF">
       <div className="inner-loginPNF">
         <div id="login-bannerPNF">
@@ -46,9 +46,11 @@ else return (
             <img src={klickrLogo} alt="" id="circlesPNF"></img>
           </div> */}
           <h4 id="to-klickr1">Page Not Found</h4>
-          <div className="linkerror">
-            <NavLink to="/" className='linkerrorText'>Click here to go home</NavLink>
-          </div>
+          <Link to='/' className="button">
+              <button to='/' className="loginform-bttns">
+                Click here to go home
+              </button>
+            </Link>
         </div>
       </div>
     </div>
