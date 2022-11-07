9acd75631f510bd02f88fd5d9286865d0111bd81
Nathan <nscott1010@gmail.com>
2022-11-07 17:50:08 -0600

    render.com setup

--- migrations/env.py
+++ migrations/env.py
@@ -3,6 +3,9 @@ from __future__ import with_statement
 import logging
 from logging.config import fileConfig
 
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
 from flask import current_app
 
 from alembic import context
@@ -75,7 +78,11 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info('No changes in schema detected.')
 
-    connectable = current_app.extensions['migrate'].db.get_engine()
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
@@ -85,9 +92,11 @@ def run_migrations_online():
             **current_app.extensions['migrate'].configure_args
         )
 
+        # Create a schema (only in production)
         if environment == "production":
             connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
 
+        # Set search path to your schema (only in production)
         with context.begin_transaction():
             if environment == "production":
                 context.execute(f"SET search_path TO {SCHEMA}")
