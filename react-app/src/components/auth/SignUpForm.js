05af096ce4bd46e01b7a48aab7eb034daf544baa
Nathan <nscott1010@gmail.com>
2023-02-22 11:28:39 -0600

    deploy

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+
 import "./SignUpForm.css";
 
 const SignUpForm = () => {
